05248b2e673d80af4c597b2246600c51d2a2b95b
thanyathep <thanyathep.hankla@gmail.com>
2025-02-21 13:20:51 +0700

    update Lab08

---
+++ Lab08_Ex02/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Person p = new Person();
+        p.Name = "Rambo";
+        p.Id = 1987;
+        p.Income = 2500;
+
+        Console.WriteLine($"Type of p is {p.GetType()}");
+        Console.WriteLine($"p.Name = {p.Name}\ttype = {p.Name.GetType()}");
+        Console.WriteLine($"p.Id = {p.Id}\ttype = {p.Id.GetType()}");
+        Console.WriteLine($"p.Income = {p.Income}\ttype = {p.Income.GetType()}");
+class Person
+{
+    public string Name { get; set; } = string.Empty;  // ป้องกัน null
+    public int Id { get; set; }                       // เปลี่ยนเป็น int
+    public int Income { get; set; }                  
+}
